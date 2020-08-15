864db05b44627977fcfe6d71e6c5b2a14589c5e0
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-15 18:37:20 -0300

    Utilizando resolvers para relacionar

--- schema-graphQL/index.js
+++ schema-graphQL/index.js
@@ -1,5 +1,26 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const profiles = [
+  { id: 1, name: 'moderator' },
+  { id: 2, name: 'administrator' },
+];
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@email.com',
+    age: '100',
+    profile_id: 1,
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    email: 'Jane@email.com',
+    age: '110',
+    profile_id: 2,
+  },
+];
+
 const typeDefs = gql`
   scalar Date
 
@@ -10,6 +31,12 @@ const typeDefs = gql`
     age: Int
     salary: Float
     vip: Boolean
+    profile: Profile
+  }
+
+  type Profile {
+    id: Int
+    name: String!
   }
 
   type Product {
@@ -25,10 +52,20 @@ const typeDefs = gql`
     now: Date
     userAuthenticated: User
     product: Product
+    users: [User]
+    user(id: ID): User
+    profiles: [Profile]
+    profile(id: Int): Profile
   }
 `;
 
 const resolvers = {
+  User: {
+    profile(user) {
+      result = profiles.filter((profile) => profile.id == user.id);
+      return result ? result[0] : null;
+    },
+  },
   Product: {
     priceDiscount(product) {
       return product.discount
@@ -60,6 +97,20 @@ const resolvers = {
         price: 6999.99,
         discount: 0.15,
       };
+    },
+    users() {
+      return users;
+    },
+    user(_, args) {
+      result = users.filter((user) => user.id == args.id);
+      return result ? result[0] : null;
+    },
+    profiles() {
+      return users;
+    },
+    profile(_, { id }) {
+      result = profiles.filter((profile) => profile.id == id);
+      return result ? result[0] : null;
     },
   },
 };
