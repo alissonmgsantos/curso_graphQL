6704c341c7e2710629aa7bf3471bbc5b3b514931
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-16 10:28:30 -0300

    Crud com mutations

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -13,4 +13,23 @@ module.exports = {
     users.push(user);
     return user;
   },
+
+  updateUser(_, args) {
+    const i = users.findIndex((user) => user.id === args.id);
+    if (i < 0) return null;
+    const user = {
+      ...users[i],
+      ...args,
+    };
+
+    users.splice(i, 1, user);
+    return user;
+  },
+
+  destoryUser(_, { id }) {
+    const i = users.findIndex((user) => user.id === id);
+    if (i < 0) return null;
+    const result = users.splice(i, 1);
+    return result ? result[0] : null;
+  },
 };
